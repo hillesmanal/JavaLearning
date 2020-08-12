4cd0249621a0688c35a90a3ceb4b0a80e698244d
MAnal <hillesmanal@gmail.com>
2020-08-12 00:59:05 -0500

    using an anonymous array

--- JavaProject/src/array/minArray.java
+++ JavaProject/src/array/minArray.java
@@ -11,12 +11,23 @@ public static  void  min(int []array){
 	System.out.println(min);
 		
 	}
+/**
+ * how to use  anonymous array 
+ */
+public static void anonymous (int arr[]) {
+	for (int i =0;i<arr.length;i++) {
+		System.out.println(arr[i]);
+	}
+}
+
 
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 int a[]= {5,6,3,9,2,4};
-min(a);
+//min(a);
+anonymous ( new int[]{4,6,8,2});
 }
 	
+
 }
